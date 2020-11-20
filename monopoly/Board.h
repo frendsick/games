5c2fb6c9c54e624b6394171eff56eff43a391572
CyberPaddy <teemu@paddy.fi>
2020-11-20 14:43:01 +0200

    Added Print function

--- monopoly/Board.h
+++ monopoly/Board.h
@@ -3,4 +3,11 @@ class Board {
     int mandatoryPayments[2] = { 4, 38 };
     int payments[40];
     std::string positions[40];
+
+    void Print() {
+      std::cout << "\n---PELILAUTA---\n";
+      for (int i=0; i<40; i++) {
+        std::cout << positions[i] << ": " << payments[i] << "€\n";
+      }
+    }
 };
