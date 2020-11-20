4c572467cc3a1698d3c90fc7cb75243958a26066
CyberPaddy <teemu@paddy.fi>
2020-11-20 13:53:34 +0200

    Added boolean variable moveBackwards

---
+++ monopoly/Card.h
@@ -0,0 +1,16 @@
+class Card {
+  private:
+    int cardID;
+    int movePosition = -1;  // Position on the board (square)
+    int payment;            // Payment on the square
+    bool mustMove;          // Does card require the player to move
+    bool moveBackwards;     // When card requires the player to move is the direction backwards
+    
+    std::string cardType;   // Sattuma or Yhteismaa
+    std::string text;       // Text on the card
+
+  public:
+    // Default constructor zeroes payment, moveBackwards and mustMove values
+    Card() { payment = 0; moveBackwards = false; mustMove = false; }
+
+};
