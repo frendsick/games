25c17359d28d473b7ed19921aff5435dd1035bb7
CyberPaddy <teemu@paddy.fi>
2020-11-20 13:03:08 +0200

    Added variables and modified constructors

--- monopoly/Player.h
+++ monopoly/Player.h
@@ -1,15 +1,20 @@
 class Player {
   private:
-    int playerID;
-    int position;       // Player's position on the game board. 0 = Start, 10 = Jail etc.
+    int playerID; 
+
+    int hotelCount;    // How many hotels player has built
+    int houseCount;    // How many houses player has built
     int money;          // How much money the player has
+    int position;       // Player's position on the game board. 0 = Start, 10 = Jail etc.
     std::string model;  // Horse, hat, dog etc.
 
   public:
-  // Default constructor zeroes player's position and sets money count to 1500
-    Player() { position = 0; money = 1500; }
+    // Constructors
+    Player() { hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
+    Player(std::string m) { model = m; hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
 
   // TODO
   void MakeMove () {
-    position++; // Just a temporary action}
+    position++; // Just a temporary action
   }
+};
