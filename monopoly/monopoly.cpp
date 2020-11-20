46052f21430c06fb229ad252aca0ce8840bf6eb6
CyberPaddy <teemu@paddy.fi>
2020-11-20 15:31:08 +0200

    Initial commit

---
+++ monopoly/monopoly.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "Board.h"
+#include "Card.h"
+#include "Player.h"
+
+#include "init.h"   // newGame() function
+
+int main() {
+  Board board;
+  Card chanceCards[16];
+  Card communityCards[16];
+  std::vector<Player> players;
+
+  newGame(board, players, chanceCards, communityCards);
+
+  board.Print();
+  for (int i=0; i<16; i++) {
+    chanceCards[i].Print();
+    communityCards[i].Print();
+  }
+
+  return 0;
+}
