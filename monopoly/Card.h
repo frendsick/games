3d3d3a6005d4733e0ca545b226083c92291d6bd0
CyberPaddy <teemu@paddy.fi>
2020-11-25 19:05:36 +0200

    Moved variables to private scope and added setters

--- monopoly/Card.h
+++ monopoly/Card.h
@@ -1,17 +1,28 @@
 class Card {
-  public:
+  private:
     int cardID;
     int movePosition = -1;  // Position on the board (square)
     int payment;            // Payment on the square
     bool mustMove;          // Does card require the player to move
     bool moveBackwards;     // When card requires the player to move is the direction backwards
     
-    std::string type;   // Sattuma or Yhteismaa
+    std::string type;       // Sattuma or Yhteismaa
     std::string text;       // Text on the card
 
+  public:
     // Constructor zeroes payment, moveBackwards and mustMove values
     Card() { payment = 0; moveBackwards = false; mustMove = false; }
     
+    // GETTERS AND SETTERS
+    void SetCardID(int id) { cardID = id; }
+    void SetMovePosition(int pos) { movePosition = pos; }
+    void SetPayment(int p) { payment = p; }
+    void SetMustMove(bool m) { mustMove = m; }
+    void SetMoveBackwards(bool m) { moveBackwards = m; }
+    void SetType(std::string t) { type = t; }
+    void SetText(std::string t) { text = t; }
+
+    // METHODS
     void Print() {
       std::cout << text << " : " << type << " : " << payment << "€";
 
