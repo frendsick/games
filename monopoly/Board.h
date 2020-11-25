637a9bdd2d5caaa5d0b520c186c2d8c0d78a2ca8
CyberPaddy <teemu@paddy.fi>
2020-11-25 18:55:38 +0200

    Moved variables to private scope and added setters

--- monopoly/Board.h
+++ monopoly/Board.h
@@ -1,8 +1,18 @@
 class Board {
-  public:
+  private:
     int mandatoryPayments[2] = { 4, 38 };
     int payments[40];
     std::string positions[40];
+
+  public:
+    // GETTERS AND SETTERS
+    void SetPayment(int pos, int amount) {
+      payments[pos] = amount;
+    }
+
+    void SetPosition(int pos, std::string text) {
+      positions[pos] = text;
+    }
 
     void Print() {
       std::cout << "\n---PELILAUTA---\n";
