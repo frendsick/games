376073a040d657a067a474ccbd4e85ceec6a7152
CyberPaddy <teemu@paddy.fi>
2020-11-26 23:50:22 +0200

    Added doubleCount variable, added getters, changed MakeMove method

--- monopoly/Player.h
+++ monopoly/Player.h
@@ -1,17 +1,21 @@
+void drawCard(int &position, Card (&cards)[16]);
+
 class Player {
   private:
     int playerID; 
+    int doubleCount;
     int hotelCount;    // How many hotels player has built
     int houseCount;    // How many houses player has built
     int money;          // How much money the player has
     int position;       // Player's position on the game board. 0 = Start, 10 = Jail etc.
+    int visitedPositions[40] = {0};
     std::string model;  // Horse, hat, dog etc.
     std::string name;
 
   public:
     // CONSTRUCTORS
-    Player() { hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
-    Player(std::string m) { model = m; hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
+    Player() { doubleCount = 0, hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
+    Player(std::string m) { model = m; doubleCount = 0, hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
 
     // GETTERS AND SETTERS
     void SetPlayerID(int id) { playerID = id; }
@@ -19,12 +23,24 @@ class Player {
     void SetHouseCount(int c) { houseCount = c; }
     void SetModel(std::string m) { model = m; }
     void SetName(std::string n) { name = n; }
+    void UpdateVisitedPositions(int p) { visitedPositions[p]++; }
 
-    int GetPosition() { return position; };
+    std::string GetName() { return name; }
+    int GetID() { return playerID; }
+    int GetPosition() { return position; }
+    int GetPositionVisitedAmount(int p) { return visitedPositions[p]; }
 
     // METHODS
-    void MakeMove(int squares) {
-      position = (position + squares) % 40;
+    void MakeMove(int diceSum, Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
+      position = (position + diceSum) % 40;
+      std::string squareName = board.GetPosition(position);
+      
+      if (squareName == "Mene vankilaan")
+        position = 10;
+      else if (squareName == "Sattuma")
+        drawCard(position, chanceCards);     // Changes players position if Sattuma-card requires it
+      else if (squareName == "Yhteismaa")
+        drawCard(position, communityCards);  // Changes players position if Yhteismaa-card requires it
     }
 
     void Print() {
