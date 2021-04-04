39cc876be4aa22ec4fb6366c4296989b504a9fd9
CyberPaddy <teemu@paddy.fi>
2021-04-04 03:56:39 +0300

    LOVE_JAIL variable defines if players stay in jail for as long as they can. Splitted some methods to submethods.

--- monopoly/Player.h
+++ monopoly/Player.h
@@ -1,11 +1,13 @@
-void drawCard(int &position, Card (&cards)[16]);
+const bool LOVE_JAIL = false;  // Are players in jail as long as possible?
 
 class Player {
   private:
+    bool inJail;
     int playerID; 
     int doubleCount;
-    int hotelCount;    // How many hotels player has built
-    int houseCount;    // How many houses player has built
+    int hotelCount;     // How many hotels player has built
+    int houseCount;     // How many houses player has built
+    int jailThrowCount; // How many times player threw dices while in jail
     int money;          // How much money the player has
     int position;       // Player's position on the game board. 0 = Start, 10 = Jail etc.
     int visitedPositions[40] = {0};
@@ -14,8 +16,8 @@ class Player {
 
   public:
     // CONSTRUCTORS
-    Player() { doubleCount = 0, hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
-    Player(std::string m) { model = m; doubleCount = 0, hotelCount = 0, houseCount = 0, money = 1500; position = 0; }
+    Player() { doubleCount = 0, hotelCount = 0, houseCount = 0, jailThrowCount = 0; money = 1500; position = 0; inJail = false; }
+    Player(std::string m) { model = m; doubleCount = 0, hotelCount = 0, houseCount = 0, jailThrowCount = 0; money = 1500; position = 0; inJail = false; }
 
     // GETTERS AND SETTERS
     void SetPlayerID(int id) { playerID = id; }
@@ -31,30 +33,88 @@ class Player {
     int GetPositionVisitedAmount(int p) { return visitedPositions[p]; }
 
     // METHODS
-    void MakeMove(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
-      // Test if player threw a pair
-      if (dices[0] == dices[1]) {
-        if (doubleCount < 2)
-          doubleCount++;
-        else {
-          position = 10;  // If player threw 3 pairs in a row, go to jail
-          doubleCount = 0;
-          return;
-        }
+    
+    // Move where card requires
+    void CardMove(Card card) {
+      if (card.GetMovePosition() < 0) { // Mene 3 askelta taaksepäin
+        position -= card.GetMovePosition();
+        return;
       }
-      else
+        
+      position = card.GetMovePosition();
+      if (position == 10) {
+        inJail = true;
         doubleCount = 0;
-      
+      }
+    }
+    
+    // Draw Sattuma or Yhteismaa card
+    void DrawCard(Card (&cards)[16]) {
+      int i = rand() % 16;
+
+      // Move where card requires
+      if (cards[i].MustMove())
+        CardMove(cards[i]);
+    }
+
+    // Calculate player's new position
+    void GetNewPosition(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
       int diceSum = dices[0] + dices[1];
       position = (position + diceSum) % 40;
       std::string squareName = board.GetPosition(position);
       
-      if (squareName == "Mene vankilaan")
+      if (squareName == "Mene vankilaan") {
+        inJail = true;
         position = 10;
+        doubleCount = 0;
+      }
       else if (squareName == "Sattuma")
-        drawCard(position, chanceCards);     // Changes players position if Sattuma-card requires it
+        DrawCard(chanceCards);     // Changes players position if Sattuma-card requires it
       else if (squareName == "Yhteismaa")
-        drawCard(position, communityCards);  // Changes players position if Yhteismaa-card requires it
+        DrawCard(communityCards);  // Changes players position if Yhteismaa-card requires it
+    }
+
+    void JailMove(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
+      // Player gets to move from jail by throwing a pair or by trying three times in a row
+      if (dices[0] == dices[1] || jailThrowCount >= 2) {
+        inJail = false;
+        jailThrowCount = 0;
+        GetNewPosition(dices, board, chanceCards, communityCards);
+        return;
+      }
+
+      jailThrowCount++;
+    }
+
+    void DoubleMove(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
+      // If player threw 3 pairs in a row, go to jail
+      if (doubleCount >= 2) {
+        position = 10; 
+        doubleCount = 0;
+        return;
+      }
+      
+      // If this was the first or second pair in a row
+      doubleCount++;
+      GetNewPosition(dices, board, chanceCards, communityCards);
+    }
+
+    void NormalMove(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]){
+        doubleCount = 0;
+        GetNewPosition(dices, board, chanceCards, communityCards);
+    }
+
+    void MakeMove(int (&dices)[2], Board &board, Card (&chanceCards)[16], Card (&communityCards)[16]) {
+      // If player is in jail
+      if (inJail && LOVE_JAIL)
+        JailMove(dices, board, chanceCards, communityCards);    // Player wants to be in jail as long as possible
+
+      // If player threw a pair
+      else if (dices[0] == dices[1])
+        DoubleMove(dices, board, chanceCards, communityCards);
+
+      else
+        NormalMove(dices, board, chanceCards, communityCards);
     }
 
     void Print() {
