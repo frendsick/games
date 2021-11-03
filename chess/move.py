db8eb7570fc526e3fe2b46158fda0801b1f9a761
CyberPaddy <teemu@paddy.fi>
2021-11-03 19:59:58 +0200

    Implemented ask_move function

--- chess/move.py
+++ chess/move.py
@@ -1,5 +1,15 @@
-def ask_move():
-    raise NotImplementedError
+import re
+# Returns given move
+def ask_move(turn: int) -> str:
+    color       = 'White' if turn % 2 == 1 else 'Black'
+    move_regex  = r'[BRQNK][a-h][1-8]|[BRQNK][a-h]x[a-h][1-8]|[BRQNK][a-h][1-8]x[a-h][1-8]|[BRQNK][a-h][1-8][a-h][1-8]|[BRQNK][a-h][a-h][1-8]|[BRQNK]x[a-h][1-8]|[a-h]x[a-h][1-8]=(B+R+Q+N)|[a-h]x[a-h][1-8]|[a-h][1-8]x[a-h][1-8]=(B+R+Q+N)|[a-h][1-8]x[a-h][1-8]|[a-h][1-8][a-h][1-8]=(B+R+Q+N)|[a-h][1-8][a-h][1-8]|[a-h][1-8]=(B+R+Q+N)|[a-h][1-8]|[BRQNK][1-8]x[a-h][1-8]|[BRQNK][1-8][a-h][1-8]|O-O|O-O-O'
+    print(f"{color}'s turn!")
+    while(True):
+        print("Give move: ", end='')
+        move = input()
+        if re.fullmatch(move_regex, move):
+            return move
+        print(f"'{move}' is not a valid move!")
 
 def make_move():
     raise NotImplementedError
