7c9ac3d022278b2b2c7e02e2329834d7a6c26044
CyberPaddy <teemu@paddy.fi>
2021-11-06 23:22:48 +0200

    Adding information of checking pieces to player object in check

--- chess/move.py
+++ chess/move.py
@@ -184,9 +184,11 @@ def move_piece(x_from: int, y_from: int, x_to: int, y_to: int, move_rule_counter
     moves = do_move(x_from, y_from, x_to, y_to, board, moves, moved_piece, captured_piece, from_square, to_square)
 
     # Update are players in check
-    player      = players[ (len(moves)+1)%2 ]
-    opponent    = players[ len(moves)%2 ]
-    if get_checking_pieces(board, opponent) != []:
+    player          = players[ (len(moves)+1)%2 ]
+    opponent        = players[ len(moves)%2 ]
+    checking_pieces = get_checking_pieces(board, opponent)
+    if checking_pieces != []:
+        opponent.checking_pieces = checking_pieces
         opponent.in_check = True
     player.in_check = False
 
