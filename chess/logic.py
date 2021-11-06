033adb9974a0ceb375ebe2ff439ee159a5f19d70
CyberPaddy <teemu@paddy.fi>
2021-11-06 23:09:45 +0200

    There is no need to check if the just moved player is in checkmate

--- chess/logic.py
+++ chess/logic.py
@@ -1,15 +1,15 @@
 from defs import Board, Move, Player
 from typing import List
 
-def is_checkmate(board: Board, white_player: Player, black_player: Player):
+def is_checkmate(board: Board, player: Player):
     # Position cannot be checkmate if none of the players are in check
-    if not white_player.in_check and not black_player.in_check:
+    if not player.in_check:
         return False
 
     print("Checking for checkmate is not implemented")
     return False
 
-def is_stalemate(board: Board, white_player: Player, black_player: Player):
+def is_stalemate(board: Board, player: Player):
     print("Checking for stalemate is not implemented")
     return False
 
@@ -17,8 +17,9 @@ def is_game_over(move_rule_counter: int, board: Board, moves: List[Move], player
     if move_rule_counter >= 50:
         print("Game over - 50 move rule")
         return True
-    white_player = players[0]
-    black_player = players[1]
-    if is_checkmate(board, white_player, black_player):
+
+    # Only the player whose turn is next can be in checkmate or stalemate
+    player = players[0] if len(moves)%2 == 0 else players[1]
+    if is_checkmate(board, player):
         return True
-    return is_stalemate(board, white_player, black_player)
+    return is_stalemate(board, player)
