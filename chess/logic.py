815ad2af86eba3df432ad99426e825ddf3b69176
CyberPaddy <teemu@paddy.fi>
2021-11-06 23:24:39 +0200

    Defined get_squares_between_pieces functions arguments

--- chess/logic.py
+++ chess/logic.py
@@ -1,10 +1,30 @@
-from defs import Board, Move, Player
+from defs import Board, Move, Piece, Player, Square
 from typing import List
+
+def get_squares_between_pieces(checking_piece: Piece, target_piece: Piece) -> List[Square]:
+    raise NotImplementedError
 
 def is_checkmate(board: Board, player: Player):
     # Position cannot be checkmate if none of the players are in check
     if not player.in_check:
         return False
+    
+    # Map the squares between king and the checking pieces
+    squares_between_king_and_checkers: List[List[Square]] = [[]] # Inner lists represent the different checking pieces
+    checking_piece_index = 0
+    for checking_piece in player.checking_pieces:
+        target_piece = board.king_locations[player.color.upper()]
+        print(target_piece)
+        squares_between_king_and_checkers[checking_piece_index].append(get_squares_between_pieces(checking_piece, target_piece))
+        checking_piece_index += 1
+
+    # Check if there is a move that escapes from the different checks
+    for y in range(8):
+        for x in range(8):
+            piece = board.squares[x][y].piece
+            print(piece.color, player.color)
+            if piece.color == player.color:
+                continue
 
     print("Checking for checkmate is not implemented")
     return False
