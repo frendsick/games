b74a9046e58239875d8b09c359b2be2e2265ad28
CyberPaddy <teemu@paddy.fi>
2021-11-06 22:21:12 +0200

    Began implementing checkmate checking

--- chess/logic.py
+++ chess/logic.py
@@ -1,11 +1,14 @@
 from defs import Board, Move, Player
 from typing import List
 
-def is_checkmate():
+def is_checkmate(board: Board, white_player: Player, black_player: Player):
+    # Position cannot be checkmate if none of the players are in check
+    if not white_player.in_check and not black_player.in_check:
+        return False
     print("Checking for checkmate is not implemented")
     return False
 
-def is_stalemate():
+def is_stalemate(board: Board, players: List[Player]):
     print("Checking for stalemate is not implemented")
     return False
 
@@ -13,6 +16,8 @@ def is_game_over(move_rule_counter: int, board: Board, moves: List[Move], player
     if move_rule_counter >= 50:
         print("Game over - 50 move rule")
         return True
-    if is_checkmate():
+    white_player = players[0]
+    black_player = players[1]
+    if is_checkmate(board, white_player, black_player):
         return True
-    return is_stalemate()
+    return is_stalemate(board, white_player, black_player)
