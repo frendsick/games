f4dd75ed7afe34e49de7e1e4824e9bf94a274e0a
CyberPaddy <teemu@paddy.fi>
2021-11-07 15:47:59 +0200

    Added variables and imports

--- chess/chess.py
+++ chess/chess.py
@@ -2,18 +2,19 @@
 import itertools
 import pygame
 from typing import List
-from defs   import Board, Move, Player, BLACK, WHITE, SCREEN_HEIGHT, SCREEN_WIDTH
+from defs   import Board, Move, Piece, Player, BLACK, WHITE, MOUSE_BUTTONS, TICKRATE, SCREEN_HEIGHT, SCREEN_WIDTH, Square
 from logic  import is_game_over
 from move   import make_move
-from utils  import print_board, print_board_background, print_board_state
+from utils  import highlight_piece, print_board, print_board_background, print_board_state
 
 def new_game():
-    game_over: bool         = False
-    whites_turn: bool       = True
-    move_rule_counter: int  = 0 # 50 move rule
-    players: List[Player] = [Player("White", "Teemu", 1337), Player("Black", "Random n00b", 420)]
-    board = Board() # Initializes the board to the starting position
-    moves: List[Move] = []
+    game_over: bool             = False
+    highlighted_piece: Piece    = None
+    whites_turn: bool           = True
+    move_rule_counter: int      = 0 # 50 move rule
+    players: List[Player]       = [Player("White", "Teemu", 1337), Player("Black", "Random n00b", 420)]
+    board                       = Board() # Initializes the board to the starting position
+    moves: List[Move]           = []
 
     # Pygame variables
     clock           = pygame.time.Clock()
