205e64d77f988768f07fa3f34860d53a31a6e01d
CyberPaddy <teemu@paddy.fi>
2021-11-12 00:29:39 +0200

    Changed is_checkmate and is_stalemate functions to not print anything

--- chess/logic.py
+++ chess/logic.py
@@ -16,17 +16,11 @@ def legal_move_found(board: Board, player: Player) -> bool:
 
 def is_checkmate(board: Board, player: Player) -> bool:
     # Position cannot be checkmate if none of the players are in check
-    if not player.in_check or legal_move_found(board, player):
-        return False
-    print("Checkmate!")
-    return True
+    return bool(player.in_check and not legal_move_found(board, player))
 
 # Stalemate is when a player is not in check but there is no legal moves
 def is_stalemate(board: Board, player: Player) -> bool:
-    if legal_move_found(board, player):
-        return False
-    print("Stalemate!")
-    return True
+    return not legal_move_found(board, player)
 
 def is_game_over(move_rule_counter: int, board: Board, moves: List[Move], players: List[Player]) -> bool:
     if move_rule_counter >= 50:
